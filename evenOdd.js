bf88d80b4a455c56b673831392951778666fe30d
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-02-23 09:21:05 +0600

    evenOdd.js

---
+++ evenOdd.js
@@ -0,0 +1,17 @@
+const num = 7;
+
+if (num % 2 === 0) {
+    console.log(`${num} is Even`);
+} else {
+    console.log(`${num} is Odd`);
+}
+
+const arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        console.log(`${arr[i]} is Even`);
+    } else {
+        console.log(`${arr[i]} is Odd`);
+    }
+}
