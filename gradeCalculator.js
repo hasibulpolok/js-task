91dfa200812431583742147a822eb45411b72cba
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-02-23 09:19:53 +0600

    gradeCalculator.js

---
+++ gradeCalculator.js
@@ -0,0 +1,16 @@
+const score = 85;
+let grade;
+
+if (score >= 90 && score <= 100) {
+    grade = "A";
+} else if (score >= 80) {
+    grade = "B";
+} else if (score >= 70) {
+    grade = "C";
+} else if (score >= 60) {
+    grade = "D";
+} else {
+    grade = "F";
+}
+
+console.log("Grade:", grade);
