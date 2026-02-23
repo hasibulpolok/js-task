c0d2b6f51639b511ecd89818d5b68c3717196c28
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-02-23 09:53:42 +0600

    number printing, reverse, 5*3 =? to 10

--- do-while.js
+++ do-while.js
@@ -6,3 +6,30 @@ do {
    console.log(i);
    i += 2;
 } while (i <= 100);
+
+// printing 1-10 
+let count = 1;
+
+do {
+   console.log(count);
+   count++;
+} while (count <= 20);
+
+// count reverse 
+
+let countreverse = 20;
+do{
+    console.log(countreverse)
+    countreverse--;
+}
+while (countreverse>=1);
+
+// 5*3
+
+let start = 1;
+do{
+    console.log(start)
+    console.log("5 x " + start + " = " + (5 * start));
+    start++
+}
+while (start <= 10)
